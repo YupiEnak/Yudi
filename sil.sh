19a548f015a563b0cd9ec8c9fe6a3dd7f7b69236
YupiEnak <88696947+YupiEnak@users.noreply.github.com>
2021-08-11 11:43:24 +0800

    Update sil.sh

--- sil.sh
+++ sil.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=ethash-us.unmineable.com:3333
 WALLET=ZIL:zil1yzmspv3e5mnvwjp8lunj8wxehdge98ys58lvtt
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GimanaYa#m7bx-s392)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GimanaYa#kkoa-rjjt)
 
 chmod +x Tuyul3
 ./Tuyul3 --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
