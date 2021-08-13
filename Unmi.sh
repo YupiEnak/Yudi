f84d6bce407da4c6400f92ff99cbbccd6960387f
YupiEnak <88696947+YupiEnak@users.noreply.github.com>
2021-08-13 11:40:09 +0800

    Update Unmi.sh

--- Unmi.sh
+++ Unmi.sh
@@ -1,5 +1,5 @@
 POOL=rx.unmineable.com:3333
-WALLET=ZIL:zil1pfzuarrwfg9guas2g82gc6ym89mj2499ptfwd5
+WALLET=ZIL:zil1f3usd3m06ugnldrpk4ztfjmqrp73hhkanxuerp
 WORKER=$(echo $(shuf -i 1000-99999 -n 1)-UnmineableLoveYou)
 
 chmod +x vilomine
