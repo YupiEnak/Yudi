08d3ac404c2cff2fc4992acef065a0b6e0bc69d8
YupiEnak <88696947+YupiEnak@users.noreply.github.com>
2021-08-14 20:13:05 +0800

    Delete ltc.sh

--- ltc.sh
+++
@@ -1,6 +0,0 @@
-POOL=ethash.unmineable.com:3333
-WALLET=LTC:ltc1qyw6ktm8c3lkesv977wkt9pmzfhul4lwer50dy8
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-UnmineableLoveYou)
-
-chmod +x PhoenixMiner
-./PhoenixMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
