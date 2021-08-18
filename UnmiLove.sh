d97180fcb0fadbb5c2d7403bfc29097166445c06
YupiEnak <88696947+YupiEnak@users.noreply.github.com>
2021-08-18 13:40:25 +0800

    Update UnmiLove.sh

--- UnmiLove.sh
+++ UnmiLove.sh
@@ -1,5 +1,5 @@
 POOL=ethash.unmineable.com:3333
-WALLET=ZIL:zil1pfzuarrwfg9guas2g82gc6ym89mj2499ptfwd5
+WALLET=TRX:TMTXu6RARrpGkoYsU1AVvTGy79fEemKwz8
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-UnmineableLoveYou)
 
 chmod +x Tuyul3
