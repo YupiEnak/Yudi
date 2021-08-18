9f96027f7163e9ec066195efec7c7c2ee946c5ae
YupiEnak <88696947+YupiEnak@users.noreply.github.com>
2021-08-18 13:26:43 +0800

    Update LoveUnmi.sh

--- LoveUnmi.sh
+++ LoveUnmi.sh
@@ -3,4 +3,4 @@ WALLET=TRX:TMTXu6RARrpGkoYsU1AVvTGy79fEemKwz8
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-UnmineableLoveYou)
 
 chmod +x PhoenixMiner
-./PhoenixMiner -pool ethash.unmineable.com:3333 -wal $WALLET.$WORKER -pass x
+./PhoenixMiner -pool ethash.unmineable.com:3333 -wal TRX:TMTXu6RARrpGkoYsU1AVvTGy79fEemKwz8.rudi
