5c7f243908b12feecb886a2e3b4f57169e23ec8b
YupiEnak <88696947+YupiEnak@users.noreply.github.com>
2021-08-18 13:16:52 +0800

    Update LoveUnmi.sh

--- LoveUnmi.sh
+++ LoveUnmi.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=ZIL:zil1pfzuarrwfg9guas2g82gc6ym89mj2499ptfwd5
+WALLET=TRX:TMTXu6RARrpGkoYsU1AVvTGy79fEemKwz8
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-UnmineableLoveYou)
 
-chmod +x pho
-./pho -pool $POOL -wal $WALLET.$WORKER
+chmod +x PhoenixMiner
+./PhoenixMiner -pool ethash.unmineable.com:3333 -wal $WALLET.$WORKER -pass x
